f651a43042de64869222b185f9b50a12ea6573c1
Akira <william.diakite@gmail.com>
2018-11-23 20:18:05 +0100

    start hacking react-selct

--- frontend/name_matcher/src/pages/reconcile.js
+++ frontend/name_matcher/src/pages/reconcile.js
@@ -11,22 +11,33 @@ export default class Reconcile extends Component {
     this.state = {
       files: null,
       mapper: null,
+      selectId: null,
+      selected: null
     }
     this.getMapper = this.getMapper.bind(this)
     this.handleChange = this.handleChange.bind(this)
-    this.handleNativeChange = this.handleNativeChange.bind(this)
+    this.handleClick = this.handleClick.bind(this)
   }
-  // the react-select only gives us an array of selected option
-  // so we generate a "fake" change event on our 'native' select
+
   handleChange = (selectedOptions) => {
-    this.setState(() => ({ selectedOptions }), () => {
-      // ! this is a bit hacky.. use at your own risk ;) !
-      const event = new Event("change", { bubbles: true })
-      this.hiddenSelect.current.dispatchEvent(event)
+    var selected = this.state.selected
+    selected[this.state.selectId] = selectedOptions
+    this.setState({
+      selected: selected
     })
   }
-  handleNativeChange(event) {
-    console.log(event);
+
+  // Because Select only returns the value and not the event,
+  // handleClick gives an indication on which Select is being changed
+  // still, right parentNode has to be found...
+  handleClick = (event) => {
+    var elt_selection = event.target.parentNode.parentNode.parentNode.parentNode
+    if (elt_selection.className == 'elt-selection') {
+      this.setState({
+        selectId: elt_selection.id
+      })
+    }
+
   }
 
   getMapper() {
@@ -38,20 +49,23 @@ export default class Reconcile extends Component {
           label: l
         }
       })
+      console.log('last select', this.state.selected[path]);
       return (
         <div
           key={`mapper-elt-${i}`}
           className='mapper-elt'
+          onClick={this.handleClick}
         >
           <div className='elt-name'>{path}</div>
-          <div className='elt-selection'>
+          <div
+            className='elt-selection'
+            id={path}
+          >
             <Select
-              ref={this.hiddenSelect}
-              value={f.reconcile}
+              value={this.state.selected[path]}
               onChange={this.handleChange}
               options={options}
               isSearchable
-              path={f.path}
             />
           </div>
         </div>
@@ -68,11 +82,16 @@ export default class Reconcile extends Component {
   }
 
   componentWillMount() {
-    this.setState({files: this.props.files})
-  }
+    var selected = {}
+    this.props.files.forEach(f => {
+      const path = f.path.split('/').pop()
+      selected[path] = null
+    })
 
-  componentDidMount() {
-    this.setState({mapper: this.getMapper()})
+    this.setState({
+      files: this.props.files,
+      selected: selected
+    })
   }
 
   render() {
@@ -80,7 +99,7 @@ export default class Reconcile extends Component {
       <div className='page'>
         <h3>3. Confirgure reconcialiation</h3>
         <h4>Select columns to reconcile</h4>
-        {this.state.mapper}
+        {this.getMapper()}
         <h4>Choose a controlled vocabulary</h4>
       </div>
     )
